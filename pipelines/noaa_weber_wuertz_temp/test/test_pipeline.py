c0a290bd4ca3050d85f4be6300ada815d90c41df
ekman-kaur <ekman.kaur@pnnl.gov>
2024-05-30 15:57:51 -0700

    Add in pipeline config file and test function for OVE (site)

--- pipelines/noaa_weber_wuertz_temp/test/test_pipeline.py
+++ pipelines/noaa_weber_wuertz_temp/test/test_pipeline.py
@@ -3,8 +3,6 @@ from pathlib import Path
 from tsdat import PipelineConfig, assert_close
 
 
-# DEVELOPER: Update paths to your configuration(s), test input(s), and expected test
-# results files.
 def test_noaa_weber_wuertz_temp_pipeline():
     config_path = Path("pipelines/noaa_weber_wuertz_temp/config/pipeline.yaml")
     config = PipelineConfig.from_yaml(config_path)
@@ -12,6 +10,19 @@ def test_noaa_weber_wuertz_temp_pipeline():
 
     test_file = "pipelines/noaa_weber_wuertz_temp/test/data/input/ca_data.csv"
     expected_file = "pipelines/noaa_weber_wuertz_temp/test/data/expected/abc.example.a1.20220424.000000.nc"
+
+    dataset = pipeline.run([test_file])
+    expected: xr.Dataset = xr.open_dataset(expected_file)  # type: ignore
+    assert_close(dataset, expected, check_attrs=False)
+
+
+def test_noaa_weber_wuertz_temp_ove_pipeline():
+    config_path = Path("pipelines/noaa_weber_wuertz_temp/config/pipeline_ove.yaml")
+    config = PipelineConfig.from_yaml(config_path)
+    pipeline = config.instantiate_pipeline()
+
+    test_file = "pipelines/noaa_weber_wuertz_temp/test/data/input/ove21010.05t.txt"
+    expected_file = "pipelines/noaa_weber_wuertz_temp/test/data/expected/ove.noaa_weber_wuertz_temp.b0.20210110.055553.nc"
 
     dataset = pipeline.run([test_file])
     expected: xr.Dataset = xr.open_dataset(expected_file)  # type: ignore
