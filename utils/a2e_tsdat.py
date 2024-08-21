6926200e25a983cc6d14a83ff0154128aa6deaf0
ekman-kaur <ekman.kaur@pnnl.gov>
2024-08-21 15:43:08 -0700

    make z_id optional

--- utils/a2e_tsdat.py
+++ utils/a2e_tsdat.py
@@ -1,7 +1,7 @@
 from tsdat.tstring import TEMPLATE_REGISTRY
 
 TEMPLATE_REGISTRY["datastream"] = (
-    "{location_id}[.{instrument}].{dataset_name}.{z_id}.{data_level}"
+    "{location_id}[.{instrument}].{dataset_name}[.{z_id}].{data_level}"
 )
 
 from tsdat import *
