28d9e379936a561f0b2740bfbe8375a828a9b8d5
ekman-kaur <ekman.kaur@pnnl.gov>
2024-08-22 12:45:02 -0700

    update to correct test file

--- pipelines/smode_iop1_saildrone/test/test_pipeline.py
+++ pipelines/smode_iop1_saildrone/test/test_pipeline.py
@@ -9,9 +9,9 @@ def test_smode_iop1_saildrone_pipeline():
     config = PipelineConfig.from_yaml(config_path)
     pipeline = config.instantiate_pipeline()
 
-    test_file = "pipelines/smode_iop1_saildrone/test/data/input/SMODE_PFC_saildrone_nonadcp_1075.nc"
+    test_file = "pipelines/smode_iop1_saildrone/test/data/input/SMODE_PFC_saildrone_nonadcp_1062.nc"
     expected_file = "pipelines/smode_iop1_saildrone/test/data/expected/smode.saildrone1.1062.a1.20211024.000000.pilot.nc"
 
     dataset = pipeline.run([test_file])
-    # expected: xr.Dataset = xr.open_dataset(expected_file)  # type: ignore
-    # assert_close(dataset, expected, check_attrs=False)
+    expected: xr.Dataset = xr.open_dataset(expected_file)  # type: ignore
+    assert_close(dataset, expected, check_attrs=False)
