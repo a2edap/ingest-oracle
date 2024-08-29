e0d24ad67f56c29cf9a0e8f78578fbdbebd89a39
ekman-kaur <ekman.kaur@pnnl.gov>
2024-08-29 11:31:30 -0700

    Updated reader function to convert time, updated global attrs and var attrs in dataset.yaml and updated test file.

--- pipelines/pilot_radiosonde/pipeline.py
+++ pipelines/pilot_radiosonde/pipeline.py
@@ -14,25 +14,7 @@ class PilotRadiosonde(IngestPipeline):
     ---------------------------------------------------------------------------------"""
 
     def hook_customize_dataset(self, dataset: xr.Dataset) -> xr.Dataset:
-        value_to_drop = cftime.DatetimeGregorian(
-            -29, 8, 17, 0, 0, 0, 0, has_year_zero=False
-        )
-        valid_time_condition = dataset.time != value_to_drop
-        ds_filtered = dataset.where(valid_time_condition, drop=True)
-
-        def convert_to_datetime(t):
-            if isinstance(t, cftime.datetime):
-                return np.datetime64(
-                    datetime(t.year, t.month, t.day, t.hour, t.minute, t.second)
-                )
-            else:
-                return t
-
-        ds_filtered["time"] = np.array(
-            [convert_to_datetime(t) for t in ds_filtered["time"].values]
-        )
-
-        return ds_filtered
+        return dataset
 
     def hook_finalize_dataset(self, dataset: xr.Dataset) -> xr.Dataset:
         return dataset
--- pipelines/pilot_radiosonde/readers.py
+++ pipelines/pilot_radiosonde/readers.py
@@ -1,9 +1,7 @@
-from datetime import datetime
 from typing import Dict, Union
-
-import numpy as np
 import xarray as xr
-
+import cftime
+import pandas as pd
 from tsdat import DataReader
 
 
@@ -11,4 +9,22 @@ class pilot_radiosonde(DataReader):
     def read(self, input_key: str) -> Union[xr.Dataset, Dict[str, xr.Dataset]]:
         ds = xr.load_dataset(input_key).swap_dims({"altitude": "time"}).reset_coords()
 
-        return ds
+        value_to_drop = cftime.DatetimeGregorian(
+            -29, 8, 17, 0, 0, 0, 0, has_year_zero=False
+        )
+        valid_time_condition = ds.time != value_to_drop
+        ds_filtered = ds.where(valid_time_condition, drop=True)
+
+        if "time" in ds_filtered.coords:
+            time_data = ds_filtered["time"].values
+
+            if isinstance(time_data[0], cftime.DatetimeGregorian):
+                time_data_str = time_data.astype(str)
+
+                time_data = pd.to_datetime(
+                    time_data_str, format="mixed", errors="coerce"
+                )
+
+                ds_filtered["time"] = ("time", time_data)
+
+        return ds_filtered
